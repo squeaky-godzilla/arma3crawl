cb84061d06923efb9067dc8dfe9af163df5b223c
squeaky-godzilla <squeaky-godzilla@github.com>
2018-09-20 07:11:12 +0100

    updated with title scoring

--- acrawl.py
+++ acrawl.py
@@ -9,8 +9,15 @@ def load_page(url):
     page = requests.get(url)
     return page.text
 
+def score_mod_title(mod_title, keywords):
+    score = 0
+    for word in str(mod_name).split(' '):
+        if word in keywords:
+            score += 1
+            return score
+
 def crawl_forum(page_text):
-    soup = bs(page_text)
+    soup = bs(page_text, features="lxml")
     result_list = []
     try:
         for link in soup.find_all("a"):
@@ -22,17 +29,23 @@ def crawl_forum(page_text):
             and not title == "None":
                 # print [title, href]
                 result_list.append({"title":title, "href":href, "score":0})
+                score = score_mod_title(title)
+
     except:
         pass
 
     return result_list
 
 
+
+
+# def crawl_mod(mod_url):
+#     score = page_analytics(topic_url)
+#     return score
+
+
+
 for url,pgvar in TARGET_WEBPAGES.items():
     for n in range(5):
         page_text = load_page(url + "?" + pgvar + "=" + str(n))
         crawl_forum(page_text)
-
-def crawl_mods(mod_list):
-    for title, href, score in mod_list.items():
-        score = page_analytics(topic_url)
